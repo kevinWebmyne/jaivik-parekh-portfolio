e7fc3ef128f1e7c156b964e3e05922c5f5ea477a
kevinpatel18 <patelkevin18102000@gmail.com>
2023-11-12 16:26:01 +0530

    remove unused function

--- src/pages/about.js
+++ src/pages/about.js
@@ -36,15 +36,6 @@ const stats = [
   },
 ];
 
-const AnimateNumbers = ({ value }) => {
-  const ref = useRef(null);
-  const motionValue = useMotionValue(0);
-  const springValue = useSpring(motionValue, { duration: 3000 });
-  const isInView = useInView(ref, { once: true });
- 
-
-  return <span ref={ref}></span>;
-};
 
 const about = () => {
   useEffect(() => {
